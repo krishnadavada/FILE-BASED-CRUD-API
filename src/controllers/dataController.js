cc22a536af976d1b2327d9bd08268c0b32da944c
Krishna-Davada <krishnadavada6@gmail.com>
2025-04-01 13:49:37 +0530

    few changes

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -3,249 +3,202 @@ const { readFile, writeFile } = require("node:fs").promises;
 const { v4: uuidv4 } = require("uuid");
 const { date } = require("../helpers/date");
 const { createResponse } = require("../helpers/response");
+const { isFileExist } = require("../helpers/fileExist");
+const { status } = require("../utils/enums");
 const sFilePath = "src/data.json";
 
+//function for get all data and shows item that is available
 async function getAllData(req, res) {
   try {
-    let oGetData = await readFile(sFilePath, "utf-8");
-    oData = JSON.parse(oGetData);
-    oData = oData.filter((i) => i.sStatus === "available");
-    return createResponse({ res, nStatusCode: 200, oData });
-  } catch (err) {
-    console.log(err.message);
-    if (err) {
-      if (err.code === "ENOENT") {
-        return createResponse({
-          res,
-          nStatusCode: 404,
-          bIsError: "File not found !",
-        });
-      } else {
-        return createResponse({
-          res,
-          nStatusCode: 500,
-          bIsError: "Internal server error !",
-        });
-      }
+
+    //check file exist or not
+    if (!(await isFileExist(sFilePath))) {
+      return createResponse( res, "NotFound", 'not_found','File');
     }
+
+    const oGetData = await readFile(sFilePath, "utf-8");
+    oData = JSON.parse(oGetData);
+
+    //filter data that is available
+    oData = oData.filter((i) => i.sStatus === (status.available || status.Available));
+    return createResponse( res, 'OK', 'fetch' , null , oData);
+
+  } 
+  catch (err) {
+
+    console.log(err);
+    return createResponse( res, 'InternalServerError','internal_err' );
+
   }
-  //return createResponse({res,nStatusCode:200,sIsMessage:'yessss'})
 }
 
+//function for get data by id
 async function getDataById(req, res) {
   try {
-    const nId = req.params.iId;
 
-    try {
-      const oGetData = await readFile(sFilePath, "utf-8");
-      let oData = JSON.parse(oGetData);
-      const nGetId = oData.findIndex((i) => i.iId === nId);
-
-      if (nGetId === -1) {
-        return createResponse({
-          res,
-          nStatusCode: 404,
-          sIsMessage: "Data not found !",
-        });
-      }
-
-      oData = oData.filter((i) => i.iId === nId);
-      return createResponse({ res, nStatusCode: 200, oData: oData });
-    } catch (err) {
-      console.log(err.message);
-      if (err) {
-        if (err.code === "ENOENT") {
-          return createResponse({
-            res,
-            nStatusCode: 404,
-            isError: "File not found !",
-          });
-        } else {
-          return createResponse({
-            res,
-            nStatusCode: 500,
-            isError: "Internal server error !",
-          });
-        }
-      }
+    //check file exist or not
+    if (!(await isFileExist(sFilePath))) {
+      return createResponse(res, 'NotFound','not_found','File' );
     }
-  } catch (err) {
-    console.log(err.message);
-    return createResponse({ res, nStatusCode: 500, isError: err.message });
+
+    const { iId } = req.params;
+
+    const oGetData = await readFile(sFilePath, "utf-8");
+    let oData = JSON.parse(oGetData);
+
+    //find index for given id
+    const nGetId = oData.findIndex((i) => i.iId === iId);
+
+    //check that data with given id is exist or not
+    if (nGetId === -1) {
+      return createResponse(res, 'NotFound', 'not_found','Data');
+    }
+
+    oData = oData.filter((i) => i.iId === iId);
+    return createResponse(res, 'OK',  'fetch',null, oData);
+
+  }
+  catch (err) {
+
+    console.log(err);
+    return createResponse(res, 'InternalServerError','internal_err');
+
   }
 }
 
+//function for add new data
 async function addData(req, res) {
   try {
+
+    //check file exist or not
+    if (!(await isFileExist(sFilePath))) {
+      return createResponse(res, 'NotFound','not_found','File');
+    }
+
     const oBody = req.body;
 
-    try {
-      const oGetData = await readFile(sFilePath, "utf-8");
-      let oData = JSON.parse(oGetData);
+    const oGetData = await readFile(sFilePath, "utf-8");
+    const oData = JSON.parse(oGetData);
 
-      const oDataExist = oData.some((i) => i.sName === oBody.sName);
+    const oDataExist = oData.some((i) => i.sName === oBody.sName);
 
-      if (oDataExist) {
-        return createResponse({
-          res,
-          nStatusCode: 400,
-          sIsMessage: "Data already exist with same product name !",
-        });
-      }
-
-      oBody.iId = uuidv4();
-      oBody.nCreatedAt = date();
-      oBody.nUpdatedAt = date();
-
-      oData.push(oBody);
-
-      await writeFile(sFilePath, JSON.stringify(oData));
-      emitter.emit("itemCreated", oBody);
-      return createResponse({
-        res,
-        nStatusCode: 201,
-        sIsMessage: "Item created successfully",
-      });
-    } catch (err) {
-      console.log(err.message);
-      if (err) {
-        if (err.code === "ENOENT") {
-          return createResponse({
-            res,
-            nStatusCode: 404,
-            isError: "File not found",
-          });
-        } else {
-          return createResponse({
-            res,
-            nStatusCode: 500,
-            isError: "Internal server error",
-          });
-        }
-      }
+    //check that same name data exist or not
+    if (oDataExist) {
+      return createResponse(res, 'BadRequest', 'name_exist');
     }
-  } catch (err) {
-    console.log(err.message);
-    return createResponse({ res, nStatusCode: 500, isError: err.message });
+
+    oBody.iId = uuidv4();
+    oBody.dCreatedAt = date();
+    oBody.dUpdatedAt = date();
+
+    oData.push(oBody);
+
+    //push data into file by writeFile
+    await writeFile(sFilePath, JSON.stringify(oData));
+
+    //when item created then below event is emit
+    emitter.emit("itemCreated", oBody);
+
+    return createResponse(res, 'Created','success','created', oBody);
+
+  } 
+  catch (err) {
+
+    console.log(err);
+    return createResponse(res, 'InternalServerError', 'internal_err');
+
   }
 }
 
+//function for update existing data
 async function updateData(req, res) {
   try {
-    const nId = req.params.iId;
+    //check file exist or not
+    if (!(await isFileExist(sFilePath))) {
+      return createResponse(res, 'NotFound','not_found','File');
+    }
+
+    //extract iId from params
+    const { iId } = req.params;
+
+    //extract request body into oBody
     const oBody = req.body;
 
-    try {
-      let oGetData = await readFile(sFilePath, "utf-8");
-      let oData = JSON.parse(oGetData);
+    const oGetData = await readFile(sFilePath, "utf-8");
+    const oData = JSON.parse(oGetData);
 
-      const nGetId = oData.findIndex((i) => i.iId === nId);
+    const nGetId = oData.findIndex((i) => i.iId === iId);
 
-      if (nGetId === -1) {
-        return createResponse({
-          res,
-          nStatusCode: 404,
-          sIsMessage: "Data not found !",
-        });
-      }
-
-      const oDataExist = oData.some((i) => i.sName === oBody.sName);
-
-      if (oDataExist) {
-        return createResponse({
-          res,
-          nStatusCode: 400,
-          sIsMessage: "Data already exist with same product name !",
-        });
-      }
-
-      oBody.iId = nId;
-      oBody.nCreatedAt = oData[nGetId].nCreatedAt;
-      oBody.nUpdatedAt = date();
-
-      oData[nGetId] = { ...oData[nGetId], ...oBody };
-
-      await writeFile(sFilePath, JSON.stringify(oData));
-
-      emitter.emit("itemUpdated", oData[nGetId]);
-      return createResponse({
-        res,
-        nStatusCode: 200,
-        sIsMessage: "Item updated successfully !",
-      });
-    } catch (err) {
-      console.log(err.message);
-      if (err) {
-        if (err.code === "ENOENT") {
-          return createResponse({
-            res,
-            nStatusCode: 404,
-            bIsError: "File not found !",
-          });
-        } else {
-          return createResponse({
-            res,
-            nStatusCode: 500,
-            bIsError: "Internal server error !",
-          });
-        }
-      }
+    //check that data with given id is exist or not
+    if (nGetId === -1) {
+      return createResponse(res, 'NotFound','not_found','Data');
     }
+
+    const oDataExist = oData.some((i) => i.sName === oBody.sName);
+
+    //check that data with same name exist or not
+    if (oDataExist && oBody.sName!==oData[nGetId].sName) {
+      return createResponse(res, 'BadRequest', 'name_exist');
+    }
+
+    oBody.iId = iId;
+    oBody.dCreatedAt = oData[nGetId].dCreatedAt;
+    oBody.dUpdatedAt = date();
+
+    oData[nGetId] = { ...oData[nGetId], ...oBody };
+
+    //push updated oData into file by writeFile
+    await writeFile(sFilePath, JSON.stringify(oData));
+
+    //when item updated then below event is emit
+    emitter.emit("itemUpdated", oData[nGetId]);
+
+    return createResponse(res, 'OK', 'success','updated', oData[nGetId]);
   } catch (err) {
-    console.log(err.message);
-    return createResponse({ res, nStatusCode: 500, bIsError: err.message });
+    console.log(err);
+    return createResponse(res, 'InternalServerError', 'internal_err');
   }
 }
 
+//function for delete data by id
 async function deleteData(req, res) {
   try {
-    const nId = req.params.iId;
 
-    try {
-      let oGetData = await readFile(sFilePath, "utf-8");
-      let oData = JSON.parse(oGetData);
-
-      const nGetId = oData.findIndex((i) => i.iId === nId);
-
-      if (nGetId === -1) {
-        return createResponse({
-          res,
-          nStatusCode: 404,
-          sIsMessage: "Data not found !",
-        });
-      }
-
-      const oDeletedData = oData.filter((i) => i.iId === nId);
-      oData = oData.filter((i) => i.iId !== nId);
-
-      await writeFile(sFilePath, JSON.stringify(oData));
-      emitter.emit("itemDeletd", oDeletedData);
-      return createResponse({
-        res,
-        nStatusCode: 200,
-        sIsMessage: "Item deleted successfully !",
-      });
-    } catch (err) {
-      console.log(err.message);
-      if (err) {
-        if (err.code === "ENOENT") {
-          createResponse({
-            res,
-            nStatusCode: 404,
-            bIsError: "File not found !",
-          });
-        } else {
-          createResponse({
-            res,
-            nStatusCode: 500,
-            bIsError: "Internal server error !",
-          });
-        }
-      }
+    //check file exist or not
+    if (!(await isFileExist(sFilePath))) {
+      return createResponse(res,'NotFound','not_found','File');
     }
-  } catch (err) {
-    console.log(err.message);
-    return createResponse({ res, nStatusCode: 500, bIsError: err.message });
+
+    //extract id from request params
+    const { iId } = req.params;
+
+    const oGetData = await readFile(sFilePath, "utf-8");
+    let oData = JSON.parse(oGetData);
+
+    const nGetId = oData.findIndex((i) => i.iId === iId);
+
+    //check that data with given id is exist or not
+    if (nGetId === -1) {
+      return createResponse(res, 'NotFound','not_found','Data');
+    }
+
+    const oDeletedData = oData.filter((i) => i.iId === iId);
+    oData = oData.filter((i) => i.iId !== iId);
+
+    //push updated oData into file by writeFile
+    await writeFile(sFilePath, JSON.stringify(oData));
+
+    //when item deleted below event is emit
+    emitter.emit("itemDeletd", oDeletedData);
+
+    return createResponse(res, 'OK','success','deleted',oDeletedData );
+
+  }
+  catch (err) {
+
+    console.log(err);
+    return createResponse(res,'InternalServerError','internal_err');
+    
   }
 }
 
