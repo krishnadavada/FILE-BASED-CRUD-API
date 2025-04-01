9220429db344dff305069c10f93696a6e547fda8
Krishna-Davada <krishnadavada6@gmail.com>
2025-04-01 13:46:49 +0530

    few chnages

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,31 +1,65 @@
-const dataController = require("../controllers/dataController");
-const dataMiddleware = require("../middlewares/validate");
 const express = require("express");
 const router = express.Router();
+const dataController = require("../controllers/dataController");
+const dataMiddleware = require("../middlewares/validate");
+const { createResponse } = require("../helpers/response");
 
+//route for get all data
 router.get("/", (req, res) => {
   dataController.getAllData(req, res);
 });
 
-router.get("/:iId", dataMiddleware.validateId, (req, res) => {
-  dataController.getDataById(req, res);
-});
+//route for get data by id
+router.get(
+  "/:iId",
+  [
+    dataMiddleware.aIdValidator, 
+    dataMiddleware.validateReq
+  ],
+  (req, res) => {
+    dataController.getDataById(req, res);
+  }
+);
 
-router.post("/", dataMiddleware.validateData, (req, res) => {
+//route for add new data
+router.post(
+  "/",
+  [
+    dataMiddleware.aDataValidator, 
+    dataMiddleware.validateReq
+  ],
+  (req, res) => {
     dataController.addData(req, res);
   }
 );
 
-router.put("/:iId", dataMiddleware.validateId, (req, res) => {
-  dataController.updateData(req, res);
-});
+//route for update existing data
+router.put(
+  "/:iId",
+  [
+    dataMiddleware.aIdValidator,
+    dataMiddleware.aDataValidator,
+    dataMiddleware.validateReq,
+  ],
+  (req, res) => {
+    dataController.updateData(req, res);
+  }
+);
 
-router.delete("/:iId", dataMiddleware.validateId, (req, res) => {
-  dataController.deleteData(req, res);
-});
+//route for delete data
+router.delete(
+  "/:iId",
+  [
+    dataMiddleware.aIdValidator, 
+    dataMiddleware.validateReq
+  ],
+  (req, res) => {
+    dataController.deleteData(req, res);
+  }
+);
 
 router.all("/*", (req, res) => {
-  res.status(404).send({ message: "Not Found" });
+  return createResponse(res, "NotFound", "Route not found !");
 });
 
 module.exports = router;
